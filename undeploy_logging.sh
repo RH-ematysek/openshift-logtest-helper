bf9925f1010a1d0c6b6b00731eab6798797ed90e
ematysek <ematysek@redhat.com>
2022-03-24 14:36:14 -0400

    Add depth to undeploy_logging.sh

--- undeploy_logging.sh
+++ undeploy_logging.sh
@@ -4,7 +4,7 @@ BRANCH=${1:-release-5.3}
 
 echo "Deploying elasticsearch-operator"
 if [ ! -d elasticsearch-operator ]; then
-  git clone https://github.com/openshift/elasticsearch-operator.git
+  git clone https://github.com/openshift/elasticsearch-operator.git --depth 1
 fi
 cd elasticsearch-operator || exit 1
 git checkout "$BRANCH"
@@ -15,7 +15,7 @@ cd ..
 echo
 echo "Deploying cluster-logging-operator"
 if [ ! -d cluster-logging-operator ]; then
-  git clone https://github.com/openshift/cluster-logging-operator.git
+  git clone https://github.com/openshift/cluster-logging-operator.git --depth 1
 fi
 cd cluster-logging-operator || exit 1
 git checkout "$BRANCH"
